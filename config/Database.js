b696ad5ff6e2e656b4e4f5eaafca3091323c7c6e
Ubuntu <ikhsanfajar17@gmail.com>
2022-08-28 18:08:53 +0000

    fix apps

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,26 @@
 import {Sequelize} from "sequelize";
-import 'dotenv/config';
+import dotenv from "dotenv";
 
-const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
-    host: process.env.DB_HOST,
+// load .env
+dotenv.config()
+
+const db_username = process.env.DATABASE_USERNAME;
+const db_password = process.env.DATABASE_PASSWORD;
+const db_database = process.env.DATABASE_DATABASE;
+const db_host = process.env.DATABASE_HOST;
+const db_port = process.env.DATABASE_PORT;
+
+const db = new Sequelize(db_database, db_username, db_password,{
+    host: db_host,
+    port: db_port,
     dialect: 'mysql'
 });
 
+try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+  
 export default db;
